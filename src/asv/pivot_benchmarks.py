d8762348eda0186f1027fa674bd1e0925c174025
Carl Anderson <canderson@anaconda.com>
2022-11-07 09:56:43 -0600

    wip: updating asv

---
+++ src/asv/pivot_benchmarks.py
@@ -0,0 +1,15 @@
+import time
+
+import pivot
+
+
+def time_import_pivot_cli():
+    from pivot import cli as _
+
+
+def time_short_sleep():
+    time.sleep(0.01)
+
+
+def time_sleep():
+    time.sleep(0.1)
